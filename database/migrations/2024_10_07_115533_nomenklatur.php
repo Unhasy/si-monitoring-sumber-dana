6b673d365ee24f43b6e076236122f546ddccb5bf
Moch Faizal Ansyori <faizmuhammad810@gmail.com>
2024-10-07 19:09:21 +0700

    initial commit & migration

---
+++ database/migrations/2024_10_07_115533_nomenklatur.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('master_nomenklatur', function (Blueprint $table) {
+            $table->id();
+            $table->integer('master_dasar_hukum_id');
+            $table->string('kategori');
+            $table->string('kode_rekening');
+            $table->string('nomenklatur');
+            $table->integer('user_pembuat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('master_nomenklatur');
+    }
+};
