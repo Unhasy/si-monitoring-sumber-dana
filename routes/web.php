74ffd451b1a792117c9329c346257f44ba9e752d
Moch Faizal Ansyori <faizmuhammad810@gmail.com>
2024-10-09 05:00:11 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\NomenklaturController;
+use App\Http\Controllers\SumberdanaController;
 
 
 Route::get('/template', function () {
@@ -20,6 +22,20 @@ Route::get('/master/user/{id}/edit', [UserController::class, 'edit'])->name('mas
 Route::put('/master/user/{id}', [UserController::class, 'update'])->name('master.user.update');
 Route::delete('/master/user/{id}', [UserController::class, 'destroy'])->name('master.user.delete');
 Route::get('/master/user/opd', [UserController::class, 'opd'])->name('master.user.opd');
+
+Route::get('/master/nomenklatur', [NomenklaturController::class, 'index'])->name('master.nomenklatur');
+Route::get('/master/nomenklatur/data', [NomenklaturController::class, 'getData'])->name('master.nomenklatur.data');
+Route::post('/master/nomenklatur', [NomenklaturController::class, 'store'])->name('master.nomenklatur.store');
+Route::get('/master/nomenklatur/{id}/edit', [NomenklaturController::class, 'edit'])->name('master.nomenklatur.edit');
+Route::put('/master/nomenklatur/{id}', [NomenklaturController::class, 'update'])->name('master.nomenklatur.update');
+Route::delete('/master/nomenklatur/{id}', [NomenklaturController::class, 'destroy'])->name('master.nomenklatur.delete');
+
+Route::get('/master/sumberdana', [SumberdanaController::class, 'index'])->name('master.sumberdana');
+Route::get('/master/sumberdana/data', [SumberdanaController::class, 'getData'])->name('master.sumberdana.data');
+Route::post('/master/sumberdana', [SumberdanaController::class, 'store'])->name('master.sumberdana.store');
+Route::get('/master/sumberdana/{id}/edit', [SumberdanaController::class, 'edit'])->name('master.sumberdana.edit');
+Route::put('/master/sumberdana/{id}', [SumberdanaController::class, 'update'])->name('master.sumberdana.update');
+Route::delete('/master/sumberdana/{id}', [SumberdanaController::class, 'destroy'])->name('master.sumberdana.delete');
 
 Route::get('/master/dasarhukum', [DashboardController::class, 'index'])->name('master.dasarhukum');
 Route::get('/master/nomenklatur', [DashboardController::class, 'index'])->name('master.nomenklatur');
